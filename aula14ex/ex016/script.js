d5fb945c4f81eb2648f6db30048b21232197ddf9
André Moraes <124800489+andre09moraes@users.noreply.github.com>
2023-03-14 10:39:45 -0300

    update ex aula 14

---
+++ aula14ex/ex016/script.js
@@ -0,0 +1,32 @@
+function contar () {
+    var ini = document.getElementById('txti')
+    var fim = document.getElementById('txtf')
+    var passo = document.getElementById('txtp')
+    var res = document.querySelector('#res')
+
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossivel contar!'
+        // alert('[ERRO] Faltam informações!')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        var i = Number(ini.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+        if ( p <= 0 ) {
+            alert('Passo invalido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f ) {
+            // contagem crescente
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        } else {
+            // contagem regressiva
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1f449}`
+            }
+        }
+        res.innerHTML += `\u{1f3c1}`
+    }
+}
