d88fdaa8ddeea3f8c862d6210761c088d65eec76
André Moraes <124800489+andre09moraes@users.noreply.github.com>
2023-03-14 19:25:08 -0300

    criação de calculadora de IMC

---
+++ meus-treinos/script.js
@@ -0,0 +1,19 @@
+function calcular() {
+    var altura = (document.getElementById('txta').value)/100
+    var peso = document.getElementById('txtp').value
+    var res = document.getElementById('res')
+
+    var imc = peso / altura ** 2
+    if (imc < 18.5){
+        res.innerHTML = 'abaixo do peso'
+    } else if (imc < 25) {
+        res.innerHTML = 'no peso ideal'
+    } else if (imc < 29) {
+        res.innerHTML = 'acima no peso'
+    } else if (imc < 40) {
+        res.innerHTML = 'Obesidade I'
+    } else {
+        res.innerHTML = 'Obesidade II'
+    }
+
+}
